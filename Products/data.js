6e0da8a05e6cd664690877cce118f63f56f7c243
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-19 16:44:01 -0700

    data.js created

--- Products/data.js
+++ Products/data.js
@@ -1,3 +1,63 @@
-const plants = {
-    
-}
+const plants = [
+    {
+        id: 1, 
+        image: 'silver-evergreen.png',
+        genus: 'Aglaonema', 
+        size: 'Medium',
+        light: 'Low light',
+        price: '60',
+
+    }, 
+    {
+        id: 2, 
+        image: 'dragon-tree.png',
+        genus: 'Dracaena marginata',
+        size: 'Medium',
+        light: 'Medium Light',
+        price: '25',
+    },
+    {
+        id: 3, 
+        image: 'mother-tongue.png',
+        genus: 'Sansevieria trifasciata',
+        size: 'Medium',
+        light: 'Medium Light',
+        price: '40',
+    },
+    {
+        id: 4, 
+        image: 'peace-lily.png',
+        genus: 'Spathiphyllum',
+        size: 'Large',
+        light: 'High Light',
+        price: '30',
+    },
+    {
+        id: 5, 
+        image: 'rubber-tree.png',
+        genus: 'Ficus elastica',
+        size: 'Medium',
+        light: 'Low Light',
+        price: '35',
+    },
+    {
+        id: 6, 
+        image: 'aloe-vera.png',
+        genus: 'Aloe barbadenis Miller',
+        size: 'Medium',
+        light: 'High Light',
+        price: '15',
+    },
+
+]
+
+
+
+li class= "plants">
+                <h3 class ="plant-name"> Silver Evergreen </h3>
+                <img class="silver-evergreen"
+                src = "assets/"/>
+                <p class="plant-genus">Aglaonema</p>
+                <p class="plant-price">$60</p>
+                <p class ="plant-size">Medium</p>
+                <p class ="light-needs"> Low Light </p>
--- test/example.test.js
+++ test/example.test.js
@@ -3,10 +3,18 @@
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('should take in a plant and return a li', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = <li class="plants">
+    <h3 class="plant-name"> Silver Evergreen </h3>
+    <img class="silver-evergreen" src="assets/"/>
+    <p class="plant-genus">Aglaonema</p>
+    <p class="plant-price">$60</p>
+    <p class="plant-size">Medium</p>
+    <p class="light-needs"> Low Light </p>
+    <button class="add-to-cart"> Add to cart</button>
+    </li>;
     
     //Act 
     // Call the function you're testing and set the result to a const
