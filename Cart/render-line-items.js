beac52995cb55b1fb710a7291fc064c941c982b8
Cierra-McDonald <cierralauren15@gmail.com>
2021-01-21 01:27:56 -0700

    created render-line func test passed

---
+++ Cart/render-line-items.js
@@ -0,0 +1,24 @@
+import { plants } from '../Products/data.js';
+import { findById } from './utils.js';
+
+export function renderTableRow(cartItem) {
+    const quantity = cartItem.quantity;
+    const plant = findById(cartItem.id, plants);
+
+    const tr = document.createElement('tr');
+
+    const nameTd = document.createElement('td');
+    const qualTd = document.createElement('td');
+    const priceTd = document.createElement('td');
+
+
+    nameTd.textContent = plant.name;
+    qualTd.textContent = quantity; 
+    priceTd.textContent = `$${quantity * plant.price}`;
+     
+    tr.append(nameTd);
+    tr.append(qualTd);
+    tr.append(priceTd);
+
+    return tr;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -3,6 +3,8 @@
 import { renderPlants } from '../Products/render-func.js';
 import { plants } from '../Products/data.js';
 import { calcItemTotal, findById } from '../Cart/utils.js';
+import { renderTableRow } from '../Cart/render-line-items.js';
+import { userCart } from '../Cart/cart-data.js';
 
 const test = QUnit.test;
 
@@ -68,4 +70,25 @@ test('calcItemTotal function should take a quantity and amount and return the to
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('should take in an order and return a table', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = 
+`<tr>\
+<td>Silver Evergreen</td>\
+<td>2</td>\
+<td>$120</td>\
+</tr>`;
+
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = renderTableRow(userCart[0], plants[0]);
+
+    console.log(actual);
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual.outerHTML, expected);
 });
